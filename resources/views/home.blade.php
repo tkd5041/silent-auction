e30b498003ad23f3093f989ba265902e6c5dc196
Toby Dunn <toby.dunn@gmail.com>
2020-10-16 17:46:18 -0700

    wording on landing screen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,10 +23,10 @@
             </div>
         </div>
     </div>
-    <div class=" row border border-success rounded ml-auto mr-auto my-5 p-3 w-50 justify contents center">
-        <h4 class="text-danger text-center">For the best iOS experience,<br>
-                    make sure your iPhone is updated to iOS 14!!</h4>
-    </div>
+    <div class="border border-success rounded m-3 p-3 w-50 mx-auto my-5">
+                <h6 class="text-danger text-center">To get the best view of the items, we suggest you view this on your computer. <br>If
+                    you’re on an iPhone, it works best if you make sure you’re updated to iOS 14. Have fun!</h6>
+            </div>
 
     <h1>Select An Event To Continue:</h1>
     <div class="row justify-content-center">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,51 +1,54 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Silent Auction</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="icon" type="image/png" href="/favicon.ico" />
+    <title>Silent Auction</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="icon" type="image/png" href="/favicon.ico" />
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="css/app.css">
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
+    <!-- Styles -->
+    <link rel="stylesheet" href="css/app.css">
+</head>
+
+<body>
+    <div class="flex-center position-ref full-height">
+        @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+            <a href="{{ url('/home') }}">Home</a>
+            @else
+            <a href="{{ route('login') }}">Login</a>
+
+            @if (Route::has('register'))
+            <a href="{{ route('register') }}">Register</a>
             @endif
+            @endauth
+        </div>
+        @endif
 
-            <div class="content">
-                <div>
-                    <img class="image-fluid title-image" src="img/sa-words.svg" alt="Silent Auction">
-                </div>
-                <div>
-                    <h3>This Site Is Invite Only</h3>
-                </div>
-                <div class="border border-success rounded m-3 p-3">
-                    <h4 class="text-danger">For the best iOS experience,<br>
-                    make sure your iPhone is updated to iOS 14!</h4>
-                </div>
+        <div class="content">
+            <div>
+                <img class="image-fluid title-image" src="img/sa-words.svg" alt="Silent Auction">
+            </div>
+            <div class="w-75 mx-auto my-3">
+                <h4>If you’re coming to the auction for the first time, just click the “register” button above to set up your bidding name and see the items!</h4>
+            </div>
+            <div class="border border-success rounded m-3 p-3 w-75 mx-auto my-5">
+                <h6 class="text-danger">To get the best view of the items, we suggest you view this on your computer. <br>If
+                    you’re on an iPhone, it works best if you make sure you’re updated to iOS 14. Have fun!</h6>
             </div>
         </div>
-        <script src="{{ asset('js/app.js') }}"></script>
-    </body>
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+
 </html>
