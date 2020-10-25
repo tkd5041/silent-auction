d592b7c51e903224fbab8819d65c5960cf47d6b8
Toby Dunn <toby.dunn@gmail.com>
2020-10-24 21:42:11 -0700

    websockets add auth to dashboard

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -39,5 +39,9 @@ class AuthServiceProvider extends ServiceProvider
         {
             return $user->hasRole('admin');
         });
+
+        Gate::define('viewWebSocketsDashboard', function ($user) {
+            return $user->hasRole('admin');
+        });
     }
 }
