ef61186158b5640a8cc8367836c1b2d48d9398eb
Toby Dunn <toby.dunn@gmail.com>
2020-11-07 21:55:22 -0700

    websockets working?

--- config/websockets.php
+++ config/websockets.php
@@ -30,7 +30,7 @@ return [
             'path' => env('PUSHER_APP_PATH'),
             'capacity' => null,
             'enable_client_messages' => true,
-            'enable_statistics' => false,
+            'enable_statistics' => true,
         ],
     ],
 
@@ -116,17 +116,13 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => 
-        // '/Users/td/.config/valet/Certificates/sa.test.TLD.crt',
-         env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null),
+        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null),
 
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-        'local_pk' => 
-        // '/Users/td/.config/valet/Certificates/sa.test.TLD.crt',
-         env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null),
+        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null),
 
         /*
          * Passphrase for your local_cert file.
